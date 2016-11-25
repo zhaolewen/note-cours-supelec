0ba33d7a0538a005459d06e1f95b8f1640e060ec
zlw <zhaolewen@gmail.com>
2016-11-25 14:02:48 +0000

    added biostat R script

---
+++ BioStats/biostat_161125.R
@@ -0,0 +1,15 @@
+m1 = matrix(rnorm(100 * 10000),100,10000)
+m2 = matrix(rnorm(100 * 10000),100,10000)
+
+m = rbind(m1,m2)
+dim(m)
+y=rep(1:2,each=100)
+
+p = apply(m,2,function(x){
+  t.test(x~y,)$p.value
+})
+
+which(p<0.05)
+
+padj_bonf <- p*ncol(m)
+sum(padj_bonf<0.05)
