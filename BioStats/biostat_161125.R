6812aed9fe09837a873818e69bf49a07dbbeeb08
zlw <zhaolewen@gmail.com>
2016-11-25 15:33:03 +0000

    p value adjusted

--- BioStats/biostat_161125.R
+++ BioStats/biostat_161125.R
@@ -6,7 +6,7 @@ dim(m)
 y=rep(1:2,each=100)
 
 p = apply(m,2,function(x){
-  t.test(x~y,)$p.value
+  t.test(x~y)$p.value
 })
 
 which(p<0.05)
@@ -15,3 +15,11 @@ padj_bonf <- p*ncol(m)
 sum(padj_bonf<0.05)
 
 padj_bonf[padj_bonf>1]=1
+
+pBH = p.adjust(p,method="BH")
+pBonf = p.adjust(p, method = "bonferroni")
+
+sum(pBH<0.05)
+
+p = data.frame(p = p, pbonf = pBonf, pBH = pBH)
+head(p, 30)
